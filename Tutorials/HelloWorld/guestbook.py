369a2ea6e3b46d5a3d693153bf7d54fbe2592910
Kendall Bingham <bingham.kendall@gmail.com>
2014-02-13 17:39:52 -0600

    Using jinja template example

--- Tutorials/HelloWorld/guestbook.py
+++ Tutorials/HelloWorld/guestbook.py
@@ -1,10 +1,16 @@
-import cgi
+import os
 import urllib
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
+import jinja2
 import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'], autoescape=True)
 
 MAIN_PAGE_FOOTER_TEMPLATE = """\
     <form action="/sign?%s" method="post">
@@ -40,7 +46,7 @@ class Greeting(ndb.Model):
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.write("<html><body>")
+
         guestbook_name = self.request.get('guestbook_name', DEFAULT_GUESTBOOK_NAME)
 
 
@@ -48,13 +54,6 @@ class MainPage(webapp2.RequestHandler):
         greetings_query = Greeting.query(
             ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
         greetings = greetings_query.fetch(10)
-
-        for greeting in greetings:
-            if greeting.author:
-                self.response.write("<b>%s</b> wrote:" % greeting.author.nickname())
-            else:
-                self.response.write("An anonymous person wrote:")
-            self.response.write("<blockquote>%s</blockquote>" % cgi.escape(greeting.content))
 
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
@@ -64,9 +63,13 @@ class MainPage(webapp2.RequestHandler):
             url_linktext = "Login"
 
         #write submission
-        sign_query_params = urllib.urlencode({'guestbook_name' : guestbook_name})
-        self.response.write(MAIN_PAGE_FOOTER_TEMPLATE % (sign_query_params, cgi.escape(guestbook_name), url, url_linktext))
-
+        template_values = {
+            'greetings': greetings,
+            'guestbook_name': urllib.quote_plus(guestbook_name),
+            'url': url,
+            'url_linktext': url_linktext, }
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
         
 
 class Guestbook(webapp2.RequestHandler):
