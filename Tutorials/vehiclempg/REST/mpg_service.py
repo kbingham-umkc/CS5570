875ff321c284a1a34722623e3ecf4073c4671a54
Kendall Bingham <klb337@umkc.edu>
2014-02-16 13:54:55 -0600

    updated teh index.html file to use ajax to retrieve, delete and add records.

--- Tutorials/vehiclempg/REST/mpg_service.py
+++ Tutorials/vehiclempg/REST/mpg_service.py
@@ -3,6 +3,7 @@ import datetime
 from protorpc import message_types
 from protorpc import remote
 from protorpc import messages
+from google.appengine.ext import ndb
 
 import mpg_db
 
@@ -10,6 +11,7 @@ class fillup(messages.Message):
 
     miles = messages.FloatField(1, required=True)
     gallons = messages.FloatField(2, required=True)
+    id = messages.StringField(3)
 
 class GetMPGRequest(messages.Message):
     limit = messages.IntegerField(1, default=20)
@@ -17,24 +19,53 @@ class GetMPGRequest(messages.Message):
 class MPGList(messages.Message):
     mpglist = messages.MessageField(fillup, 1, repeated=True)
 
+class Deletemsg(messages.Message):
+    id = messages.StringField(1, required=True)
+
 class PostService(remote.Service):
 
     #add remote dectorator
-    @remote.method(fillup, message_types.VoidMessage)
+    @remote.method(Deletemsg, MPGList)
+    def delete_mpg(self, request):
+        entkey = ndb.Key(urlsafe=request.id)
+        entkey.delete()
+
+        query = mpg_db.Miles_And_Fuel.query()
+        mpglist = []
+        for mpg in query.fetch(100):
+            mpgItem = fillup(miles = mpg.miles, gallons=mpg.gallons, id=mpg.key.urlsafe())
+            mpglist.append(mpgItem)
+        
+        return MPGList(mpglist=mpglist)
+
+
+    #add remote dectorator
+    @remote.method(fillup, MPGList)
     def post_mpg(self, request):
 
         note = mpg_db.Miles_And_Fuel(miles=request.miles, gallons=request.gallons)
         note.put()
-        return message_types.VoidMessage()
 
+
+        query = mpg_db.Miles_And_Fuel.query()
+        mpglist = []
+        for mpg in query.fetch(100):
+            mpgItem = fillup(miles = mpg.miles, gallons=mpg.gallons, id=mpg.key.urlsafe())
+            mpglist.append(mpgItem)
+        
+        mpglist.append(fillup(miles = request.miles, gallons = request.gallons))
+        
+        return MPGList(mpglist=mpglist)
+    
+    
     #Do a get on the site.
     @remote.method(GetMPGRequest, MPGList)
     def get_mpglist(self, request):
         
         query = mpg_db.Miles_And_Fuel.query()
         mpglist = []
-        for mpg in query.fetch(10):
-            mpgItem = fillup(miles = mpg.miles, gallons=mpg.gallons)
+        for mpg in query.fetch(100):
+            mpgItem = fillup(miles = mpg.miles, gallons=mpg.gallons, id=mpg.key.urlsafe())
             mpglist.append(mpgItem)
             
         
