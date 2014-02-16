3e543c818e790ad3409d635555e0243cff4f0ad9
Kendall Bingham <klb337@umkc.edu>
2014-02-15 19:31:00 -0600

    Got a Get to work on the data.  Still haven't completed a UI on this.

--- Tutorials/vehiclempg/REST/mpg_service.py
+++ Tutorials/vehiclempg/REST/mpg_service.py
@@ -11,6 +11,12 @@ class fillup(messages.Message):
     miles = messages.FloatField(1, required=True)
     gallons = messages.FloatField(2, required=True)
 
+class GetMPGRequest(messages.Message):
+    limit = messages.IntegerField(1, default=20)
+
+class MPGList(messages.Message):
+    mpglist = messages.MessageField(fillup, 1, repeated=True)
+
 class PostService(remote.Service):
 
     #add remote dectorator
@@ -20,3 +26,16 @@ class PostService(remote.Service):
         note = mpg_db.Miles_And_Fuel(miles=request.miles, gallons=request.gallons)
         note.put()
         return message_types.VoidMessage()
+
+    #Do a get on the site.
+    @remote.method(GetMPGRequest, MPGList)
+    def get_mpglist(self, request):
+        
+        query = mpg_db.Miles_And_Fuel.query()
+        mpglist = []
+        for mpg in query.fetch(10):
+            mpgItem = fillup(miles = mpg.miles, gallons=mpg.gallons)
+            mpglist.append(mpgItem)
+            
+        
+        return MPGList(mpglist=mpglist)
